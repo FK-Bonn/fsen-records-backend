d759f11fafbabe2768c1a9bedb2b4a69b6edabb4
HSZemi <hszemi@users.noreply.github.com>
2022-04-30 17:57:25 +0200

    Add CORS middleware

--- main.py
+++ main.py
@@ -2,14 +2,31 @@
 
 import uvicorn
 from fastapi import FastAPI
+from starlette.middleware.cors import CORSMiddleware
 
 import fsen
 import users
+
+origins = [
+    "http://localhost",
+    "http://localhost:8080",
+    "https://fsen.datendrehschei.be",
+]
+
 
 app = FastAPI(
     openapi_url="/api/v1/openapi.json",
     docs_url="/api/v1/docs",
     redoc_url="/api/v1/redoc",
+)
+
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
 app.include_router(
