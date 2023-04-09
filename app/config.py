f02a53d262a08ccdbce8bbed600ca48ca3dbc9d5
HSZemi <hszemi@users.noreply.github.com>
2023-04-09 23:17:27 +0200

    Fix default db path

--- app/config.py
+++ app/config.py
@@ -11,7 +11,7 @@ class Config:
         else "1883eb9a04f787018d99ff7dceb4ade9af17cf91d70593336e13a40630dd18c5"
     DB_CONNECTION_STRING = os.environ["DB_CONNECTION_STRING"] \
         if "DB_CONNECTION_STRING" in os.environ \
-        else 'sqlite:///' + str(Path(__file__).resolve().parent / 'data' / 'data.db')
+        else 'sqlite:///' + str(Path(__file__).resolve().parent.parent / 'data' / 'data.db')
     BASE_DATA_DIR = Path(os.environ["BASE_DATA_DIR"]) \
         if "BASE_DATA_DIR" in os.environ \
         else Path(__file__).parent.resolve().parent / 'data' / 'files'
