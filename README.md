3dde9d465693dbc774bb63cf6f40ab775fbb4ced
HSZemi <hszemi@users.noreply.github.com>
2025-10-16 14:41:28 +0200

    Update setup instructions

--- README.md
+++ README.md
@@ -4,21 +4,28 @@ Backend for fsen-records
 
 ## Setup
 
-```
+```shell
 virtualenv -p python3 venv
 source venv/bin/activate
 pip install -r requirements.txt
 ```
 
+Create an admin user with the default Database:
+
+```shell
+source venv/bin/activate
+./create-user.py data/data.db admin password
+```
+
 ## Running tests
 
-```
+```shell
 PYTHONPATH=. pytest
 ```
 
 ## Running (development)
 
-```
+```shell
 source venv/bin/activate
-./main.py
+TEST_FAKE_SSO_ACTIVE=yup fastapi dev
 ```
--- create-user.py
+++ create-user.py
@@ -1,30 +1,40 @@
 #! /usr/bin/env python3
 
 import sys
+from argparse import ArgumentParser
 from pathlib import Path
 
 from sqlalchemy import create_engine
 from sqlalchemy.orm import Session
 from sqlalchemy_utils import database_exists, create_database
 
-from app.database import Base, User, get_password_hash
+from app.database import Base, User, get_password_hash, UserPassword, AdminPermission
 
 
 def main():
-    db_file, username, password = sys.argv[1:]
-    connection_string = 'sqlite:///' + str(Path(db_file).resolve())
+    parser = ArgumentParser('Create Admin user')
+    parser.add_argument('db_file', type=Path)
+    parser.add_argument('username', type=str)
+    parser.add_argument('password', type=str)
+    args = parser.parse_args()
+    connection_string = 'sqlite:///' + str(args.db_file.resolve())
     print(connection_string)
     if not database_exists(connection_string):
         create_database(connection_string)
     engine = create_engine(connection_string)
     Base.metadata.create_all(engine)
     session = Session(engine)
+    items = []
     user = User()
-    user.username = username
-    user.hashed_password = get_password_hash(password)
-    user.admin = True
+    user.username = args.username
+    user.full_name = args.username
     user.created_by = 'root'
-    session.add_all([user])
+    items.append(user)
+    user_password = UserPassword(user=args.username, hashed_password=get_password_hash(args.password))
+    items.append(user_password)
+    admin_permission = AdminPermission(user=args.username, created_by='root')
+    items.append(admin_permission)
+    session.add_all(items)
     session.commit()
     session.close()
 
